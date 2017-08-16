e08894b9824625fb860d3c42a941009670e720d1
Anthony-g2 <ant.greenwell2@gmail.com>
2017-08-16 02:57:22 -0700

    completed +bonus login.js

--- starter-code/login.js
+++ starter-code/login.js
@@ -3,11 +3,19 @@ var userLogin = {
   userName: 'McLovin81',
   userPass: 'Hawaii<3'
 };
-var x = prompt('Enter password for '+userLogin.userName);
+var tries = [];
+var y = 3;
+var x = prompt(`Enter password for ${userLogin.userName}`);
 while (x != userLogin.userPass) {
   if (x == userLogin.userPass) {
     break;
-  } else {
-    x = prompt('Incorrect, enter password for '+userLogin.userName);
+  }
+  x = prompt(`Incorrect, enter password for ${userLogin.userName}
+    Remaining attempts: ${y}`);
+  tries.push('');
+  y--;
+  if (tries.length == y) {
+    alert('Too many attempts, goodbye.');
+    break
   }
 };
