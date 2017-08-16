8367ae62d015b46c07c7961fcf58fd87a34e3e62
Anthony-g2 <ant.greenwell2@gmail.com>
2017-08-16 02:38:40 -0700

    completed

--- starter-code/login.js
+++ starter-code/login.js
@@ -1,1 +1,13 @@
 console.log("login.js loaded");
+var userLogin = {
+  userName: 'McLovin81',
+  userPass: 'Hawaii<3'
+};
+var x = prompt('Enter password for '+userLogin.userName);
+while (x != userLogin.userPass) {
+  if (x == userLogin.userPass) {
+    break;
+  } else {
+    x = prompt('Incorrect, enter password for '+userLogin.userName);
+  }
+};
