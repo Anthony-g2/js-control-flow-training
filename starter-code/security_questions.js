ec96ecf333c67bf78bad8ced1f4a3386dd45378d
Anthony-g2 <ant.greenwell2@gmail.com>
2017-08-16 02:08:42 -0700

    completed

--- starter-code/security_questions.js
+++ starter-code/security_questions.js
@@ -1,1 +1,25 @@
 console.log("security_questions.js loaded");
+let securityQuestions = [
+  {
+    question:"Mother's Maiden Name",
+    expectedAnswer: "Doe"
+  },
+  {
+    question:"Pet's Name",
+    expectedAnswer: "Fluffy"
+  },
+  {
+    question:"Birth City",
+    expectedAnswer: "Atlantis"
+  }
+];
+/* src = "https://www.reddit.com/r/learnprogramming/comments/16sb71/javascript_how_do_i_repeat_a_prompt_until_a/" */
+for (var i = 0;i<securityQuestions.length;i++) {
+  var x = prompt(securityQuestions[i].question)
+  if (x != securityQuestions[i].expectedAnswer) {
+    alert('Incorrect, refresh the page and try again.');
+    break
+  } else {
+    x;
+  }
+};
